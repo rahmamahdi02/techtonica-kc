3b00e1f6a3cd2f88d456162ca53baf7fa77d1d6e
TPL1122_18 <tpl1122_18@TPL1122-18s-MacBook-Pro.local>
2023-02-24 09:05:54 -0800

    I am adding in routes for index.html, about me, contactme, and 404

--- server/index.js
+++ server/index.js
@@ -18,11 +18,19 @@ app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, "../client/index.html"))
 } )
 
+
 //router for about me
 app.get('/aboutme', (req, res)=>{ //then need to create html
     // res.json('Hello this will be a single page');// testing should give us this text on the browser when we do localhost:8080
     res.sendFile(path.join(__dirname, "../client/about.html")) //making sure the path is about me
 } )
+
+//router for about me
+app.get('/aboutme', (req, res)=>{ //then need to create html
+    // res.json('Hello this will be a single page');// testing should give us this text on the browser when we do localhost:8080
+    res.sendFile(path.join(__dirname, "../client/contactme.html")) //making sure the path is about me
+} )
+
 
 //anything that user calls thats not a valid path: 
 //must be last router of app kind of like a switch or an else
@@ -34,5 +42,5 @@ app.use((req,res)=>{
 // app.post("/", (req,res) => {
 //     res.send(req.body)
 // }) 
-app.listen(PORT, console.log(`Hello, this is my server in port ${PORT}`)); //testing to see if port we are on is working
+app.listen(PORT, console.log(`Jambo, this is my server in port ${PORT}`)); //testing to see if port we are on is working
 
